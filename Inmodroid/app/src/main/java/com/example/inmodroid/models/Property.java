8455737ae48e591a1ec8f432b291f92d00b2f216
Juanma Vela <jm.vela94@gmail.com>
2019-02-24 16:53:08 +0100

    terminando entrega de hoy

---
+++ Inmodroid/app/src/main/java/com/example/inmodroid/models/Property.java
@@ -0,0 +1,151 @@
+package com.example.inmodroid.models;
+
+public class Property {
+
+    private String ownerId;
+    private String title;
+    private String description;
+    private String price;
+    private String rooms;
+    private String size;
+    private String categoryId;
+    private String address;
+    private String zipcode;
+    private String city;
+    private String province;
+    private String loc;
+
+
+    public Property (){}
+
+
+    public Property(String ownerId, String title, String description, String price, String rooms, String size, String categoryId, String address, String zipcode, String city, String province, String loc) {
+        this.ownerId = ownerId;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.rooms = rooms;
+        this.size = size;
+        this.categoryId = categoryId;
+        this.address = address;
+        this.zipcode = zipcode;
+        this.city = city;
+        this.province = province;
+        this.loc = loc;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(String rooms) {
+        this.rooms = rooms;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getLoc() {
+        return loc;
+    }
+
+    public void setLoc(String loc) {
+        this.loc = loc;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Property{" +
+                "ownerId='" + ownerId + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", price='" + price + '\'' +
+                ", rooms='" + rooms + '\'' +
+                ", size='" + size + '\'' +
+                ", categoryId='" + categoryId + '\'' +
+                ", address='" + address + '\'' +
+                ", zipcode='" + zipcode + '\'' +
+                ", city='" + city + '\'' +
+                ", province='" + province + '\'' +
+                ", loc='" + loc + '\'' +
+                '}';
+    }
+}
--- Inmodroid/app/src/main/java/com/example/inmodroid/ui/DashboardActivity.java
+++ Inmodroid/app/src/main/java/com/example/inmodroid/ui/DashboardActivity.java
@@ -1,5 +1,6 @@
-package com.example.inmodroid;
+package com.example.inmodroid.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,7 +14,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+import com.example.inmodroid.R;
+
+public class DashboardActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
@@ -80,8 +83,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.goLogin) {
+            startActivity(new Intent(DashboardActivity.this,LoginActivity.class));
+            
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
